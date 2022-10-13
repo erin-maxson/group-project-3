c848c699433ffd2fa9e3bbd45dbadc173c19d593
erin-maxson <erin@cohatch.com>
2022-10-12 19:00:14 -0700

    updating map component + app js

--- happy-camper/src/components/Map/Map.jsx
+++ happy-camper/src/components/Map/Map.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYWlybWF4MTQiLCJhIjoiY2w4amZrbXhvMDY4ODN3bzJtbnpjNTJsMSJ9.K1O2yAfN9AJ8eg32-XuENA';
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWlybWF4MTQiLCJhIjoiY2w4amZrbXhvMDY4ODN3bzJtbnpjNTJsMSJ9.K1O2yAfN9AJ8eg32-XuENA';
 
 const Map = () => {    
         const mapContainer = useRef(null);
