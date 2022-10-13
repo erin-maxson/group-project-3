c6058bce83884b54d2dcedc3aa957ae78dec9be2
erin-maxson <erin@cohatch.com>
2022-10-12 18:15:53 -0700

    navigation setup

---
+++ happy-camper/src/components/Nav/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Nav.css'
+
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav className='container.header_container'>
+        <a href="#Signup">Signup</a>
+        <a href="#Login">Login</a>
+    </nav>
+  )
+}
+
+export default Nav
