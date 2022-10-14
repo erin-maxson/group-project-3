2c2d554f8d87e04fbf6f69c7ac00f7fc31c2f1e1
erin-maxson <erin@cohatch.com>
2022-10-14 16:25:28 -0700

    adding to do notation for login

--- happy-camper/src/App.js
+++ happy-camper/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Nav/Nav'
 import Nav from './components/Header/Header'
 import Signup from './components/Signup/Signup'
+// need to import login item here when ready to connect to backend
 import Map from './components/Map/Map'
 
 export default function App() {
