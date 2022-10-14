5d485fd285075b8bedc79d25ad1ab32137de2a98
fuaus <kotingfunk@gmail.com>
2022-10-14 18:46:26 -0400

    typeDefs.js input and mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,12 +5,19 @@ const typeDefs =gql`
  input SaveLocationInput {
     username: String!
     title: String!
+    description: String!
+    rating: Int!
+    latitude: Int!
+    longitude: Int!
  }
  
  type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-
-
+    saveLocation(location: SaveLocationInput): User
+    removeLocation(_id: ID!): Location
+    updateLocation(_id: ID!, title: String!, rating: String!): Location
  }
 `;
+
+module.exports = typeDefs;
