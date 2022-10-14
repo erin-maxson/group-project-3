212c91d7cb2dbf0bf279324efb86df575463a0e4
fuaus <kotingfunk@gmail.com>
2022-10-14 17:58:16 -0400

    adding schemas files and folder

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,15 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs =gql`
+ 
+ input SaveLocationInput {
+    username: String!
+    title: String!
+ }
+ 
+ type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+
+ }
+`;
