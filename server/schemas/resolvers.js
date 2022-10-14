a6c0592ce151b725c8a25e3fe56495ecf8cc5b12
Owen Newlove <owen.newlove@gmail.com>
2022-10-14 19:50:14 -0400

    created the three consts AuthenticationError, {User, Location}, and signToken. Then created a const called resolvers with Query in it. Then completed the users, user, locations, location, and me from the typeDef's. This was all done in the resolvers file

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,34 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Location } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+
+    Query: {
+
+        users: async () => {
+            return User.find().populate('savedLocations')
+        },
+        user: async(parent, { username}) => {
+            return User.findOne({username}).populate('savedLocations')
+        },
+        locations: async (parent, {username}) => {
+            const params = username ? {username} : {};
+            return Location.find(params).sort({createdAt: -1}) //needs to be fixed since it is a map
+        },
+        location: async (parent, {locationId}) => {
+            return Location.findOne({_id: locationId})
+        },
+        me: async (parent, args, context) => {
+            if (context.user) {
+              return User.findOne({ _id: context.user._id }).populate('thoughts');
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        }
+
+
+
+    }
+
+
+}
