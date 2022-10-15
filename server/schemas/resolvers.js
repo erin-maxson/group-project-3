2790b70f02629c17b1cbe0fbc5d6d3fcfacad612
Owen Newlove <owen.newlove@gmail.com>
2022-10-15 11:49:02 -0400

    created saveLocation and updateLocation within the mutations of the resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,24 +12,60 @@ const resolvers = {
         // user: async(parent, { username}) => {
         //     return User.findOne({username}).populate('savedLocations')
         // },
-        savedLocations: async (parent, {username}) => {
-            const params = username ? {username} : {};
-            return Location.find(params).sort({createdAt: -1}) 
+        savedLocations: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Location.find(params).sort({ createdAt: -1 })
         },
-        locations: async ()=> {
+        locations: async () => {
             return Location.find()
         },
-        location: async (parent, {locationId}) => {
-            return Location.findOne({_id: locationId})
+        location: async (parent, { locationId }) => {
+            return Location.findOne({ _id: locationId })
         },
         me: async (parent, args, context) => {
             if (context.user) {
-              return User.findOne({ _id: context.user._id }).populate('savedLocations');
+                return User.findOne({ _id: context.user._id }).populate('savedLocations');
             }
             throw new AuthenticationError('You need to be logged in!');
         }
 
 
+
+    },
+
+    Mutation: {
+
+
+
+
+
+
+
+
+
+        saveLocation: async (parent, args, context) => {
+            if (!context.user) {
+
+                throw new AuthenticationError('You need to be logged in!')
+
+            }
+            await User.findOneandUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedLocations: args.location } },
+                {new: true, runValidators: true}
+            )
+            return location
+
+        },
+
+        updateLocation: async () => {
+            await Location.findOneandUpdate(
+                {_id: location._id},
+                {$push: {location: args.updateLocation}},
+                {new: true, runValidators: true}
+            )
+            return location
+        }
 
     }
 
