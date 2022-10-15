baa5997113de334f673d9d6e516269882f9cff65
Erin Maxson <103834510+erin-maxson@users.noreply.github.com>
2022-10-15 10:19:51 -0700

    Revert "Jlr/mapbox search box"

--- happy-camper/src/components/Header/Header.jsx
+++ happy-camper/src/components/Header/Header.jsx
@@ -4,18 +4,8 @@ import LOGO from '../../assets/happycamperlogo.png'
 
 const Header = () => {
   return (
-<<<<<<< HEAD
-    <div className='container.header_container'>
-      <img src={LOGO} alt="happycamper logo" />
-      <mapbox-search-box
-        access-token="<your access token here>"
-        proximity="0,0"
-      >
-      </mapbox-search-box>
-=======
     <div className='header'>
         <img src={LOGO} alt="happycamper logo" />
->>>>>>> develop
     </div>
   )
 }
