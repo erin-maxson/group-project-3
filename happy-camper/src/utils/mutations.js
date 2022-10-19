5d75c8bea2a89d1ac11450d6e7be5692bdc121fc
Owen Newlove <owen.newlove@gmail.com>
2022-10-19 18:50:13 -0400

    finished the mutations file within the utils folder of the client side

--- happy-camper/src/utils/mutations.js
+++ happy-camper/src/utils/mutations.js
@@ -18,17 +18,72 @@ mutation Login($email: String!, $password: String!) {
 
 
 
-export const ADD_LOCATION = gql`
-
-
-
-
-`
 
 
 
 export const ADD_USER = gql`
 
+mutation AddUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
 
+
+
+`;
+
+export const ADD_LOCATION = gql`
+
+mutation SaveLocation {
+    saveLocation {
+      _id
+      username
+      email
+    }
+  }
+
+
+
+`;
+
+
+
+
+export const REMOVE_LOCATION = gql`
+
+
+mutation RemoveLocation($locationId: ID!) {
+    removeLocation(locationId: $locationId) {
+      _id
+      username
+      title
+      description
+      rating
+      latitude
+      longitude
+    }
+  }
+
+`;
+
+export const UPDATE_LOCATION = gql`
+
+mutation UpdateLocation($locationId: ID!) {
+    updateLocation(locationId: $locationId) {
+      _id
+      username
+      title
+      description
+      rating
+      latitude
+      longitude
+    }
+  }
 
 `;
