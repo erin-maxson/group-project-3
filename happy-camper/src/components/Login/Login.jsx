f1d0c002f3acf3812dc0f96f0f20b7bcb83bb7ec
Owen Newlove <owen.newlove@gmail.com>
2022-10-19 20:47:43 -0400

    added link to send user back to homepage after a successful login

--- happy-camper/src/components/Login/Login.jsx
+++ happy-camper/src/components/Login/Login.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import logo from '../../assets/vanlogo.png'
-import {Link} from 'react-router-dom'
-import {useMutation} from '@apollo/happy-camper'
-import {LOGIN} from '../../utils/mutations'
+import { Link } from 'react-router-dom'
+import { useMutation } from '@apollo/happy-camper'
+import { LOGIN } from '../../utils/mutations'
 
 import Auth from '../../utils/auth'
 
 
 const Login = (props) => {
 
-    const [formState, setFormState] = useState({ username: '', email: '', password: ''})
-    const [login, {error, data}] = useMutation(LOGIN)
+    const [formState, setFormState] = useState({ username: '', email: '', password: '' })
+    const [login, { error, data }] = useMutation(LOGIN)
 
     const handleChange = (event) => {
-        const {name, value} = event.target
+        const { name, value } = event.target
 
         setFormState({
             ...formState,
@@ -25,67 +25,88 @@ const Login = (props) => {
         event.preventDefault();
         console.log(formState);
         try {
-          const { data } = await login({
-            variables: { ...formState },
-          });
-    
-          Auth.login(data.login.token);
+            const { data } = await login({
+                variables: { ...formState },
+            });
+
+            Auth.login(data.login.token);
         } catch (e) {
-          console.error(e);
+            console.error(e);
         }
-    
+
         // clear form values
         setFormState({
-          username: '',
-          email: '',
-          password: '',
+            username: '',
+            email: '',
+            password: '',
         });
-      };
+    };
 
 
     return (
         <div>
             <div className='login-container'>
+
+
+
                 <div className='logo'>
                     <img src={logo} alt='small van logo' />
                 </div>
 
                 <h2>Login</h2>
 
-                <form onSubmit={handleFormSubmit}>
-                    <p>Username</p>
-                    <input 
-                    type="text" 
-                    placeholder="username" 
-                    name="username"
-                    value={formState.username}
-                    onChange={handleChange}
+                <div className='card-body'>
+                    {data ? (
+                        <p>
+                            Success! You may now head{' '}
+                            <Link to="/">back to the homepage.</Link>
+                        </p>
+                    ) : (
 
-                    />
-                    <p>Email</p>
-                    <input 
-                    type="email" 
-                    placeholder="email"
-                    name="email"
-                    value={formState.password}
-                    onChange={handleChange}
-                     />
+                        <form onSubmit={handleFormSubmit}>
+                            <p>Username</p>
+                            <input
+                                type="text"
+                                placeholder="username"
+                                name="username"
+                                value={formState.username}
+                                onChange={handleChange}
+
+                            />
+                            <p>Email</p>
+                            <input
+                                type="email"
+                                placeholder="email"
+                                name="email"
+                                value={formState.password}
+                                onChange={handleChange}
+                            />
 
 
-                    <p>Password</p>
-                    <input 
-                    type="password" 
-                    placeholder="password" 
-                    name="password"
-                    value={formState.password}
-                    onChange={handleChange}
-                    />
-                    <button className='signupBtn' 
-                    type="submit"
-                    >
-                        On the road again...
-                        </button>
-                </form>
+                            <p>Password</p>
+                            <input
+                                type="password"
+                                placeholder="password"
+                                name="password"
+                                value={formState.password}
+                                onChange={handleChange}
+                            />
+                            <button className='signupBtn'
+                                type="submit"
+                            >
+                                On the road again...
+                            </button>
+                        </form>
+
+                    )}
+
+                    {error && (
+                        <div className="my-3 p-3 bg-danger text-white">
+                            {error.message}
+                        </div>
+                    )}
+
+                </div>
 
             </div>
         </div>
