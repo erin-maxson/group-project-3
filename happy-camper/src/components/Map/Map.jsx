2535b4605abb9a4e7853e976663bda53c2505586
fuaus <kotingfunk@gmail.com>
2022-10-23 13:52:03 -0400

    moving pins back

--- happy-camper/src/components/Map/Map.jsx
+++ happy-camper/src/components/Map/Map.jsx
@@ -86,7 +86,7 @@ const SearchableMap = () => {
             <Marker longitude={p.longitude} latitude={p.latitude} anchor="bottom" >
               <FaMapMarkerAlt style={{ fontSize: viewport.zoom * 7, color: '#f39200' }} />
             </Marker>
-            {/* <Popup className='popup' longitude={p.longitude} latitude={p.latitude}
+            <Popup className='popup' longitude={p.longitude} latitude={p.latitude}
               anchor="bottom"
               closeButton={true}
               closeOnClick={true}
@@ -98,7 +98,7 @@ const SearchableMap = () => {
                 <p className='review'>{p.rating}/5 stars</p>
                 <button className="addBtn" href='#'>Update this pin!</button>
               </div>
-            </Popup> */}
+            </Popup>
           </div>
         ))}
 
